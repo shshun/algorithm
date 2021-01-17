ca06afac973cc54b5777596d6bdec6dcb4792e4b
shshun <skyworking1@gmail.com>
2021-01-17 22:01:52 +0900

    dp

---
+++ asdf.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+
+//10:22
+
+
+
+int arr[701][701];
+int M, N;
+vector<int> v;
+
+
+void printing() {
+
+	int x, y;
+	int idx = 0;
+
+	for (x = M - 1; x >= 0; x--) {
+		arr[x][0] += v[idx++];
+	}
+	for (y = 1; y < M; y++) {
+
+		arr[0][y] += v[idx++];
+		for (int i = 1; i < M; i++) {
+			arr[i][y] = arr[0][y];
+		}
+
+	}
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < M; j++) {
+			cout << arr[i][j] + 1 << " ";
+		}cout << "\n";
+	}
+
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> M >> N;
+
+
+	v.assign(2 * M + 1, 0);
+
+	for (int i = 0; i < N; i++) {
+
+		int one, two, zero;
+		cin >> zero >> one >> two;
+
+		for (int j = zero; j < zero + one; j++)
+			v[j] += 1;
+
+		for (int j = one + zero; j < 2 * M - 1; j++)
+			v[j] += 2;
+
+	}
+
+
+	printing();
+	return 0;
+}
---
+++ 백준15486(퇴사2).cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+//8:37
+
+typedef pair<int, int> p;
+int N;
+int dp[150010];
+vector<p> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N;
+	v.push_back({ 0,0 });
+	for (int i = 1; i <=N; i++) {
+		int a, b;
+		cin >> a >> b;
+
+		v.push_back({ a,b });
+	}
+	//v.push_back({ 0,0 });
+	int answer = 0;
+	for (int i = 1; i <= N+1; i++) {
+		answer = max(answer, dp[i]);
+		if (i+ v[i].first > N + 1) continue;
+		dp[i + v[i].first] = max(answer+v[i].second,dp[i+v[i].first]);
+	}
+
+	cout << answer <<'\n';
+	return 0;
+
+
+}
